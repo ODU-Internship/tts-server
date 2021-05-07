f1fdf5335e90cc4ee03a8ff26da4f13a4438a9f1
Likith <likithsrinath2000@gmail.com>
2021-05-07 09:55:41 +0530

    Initial Server

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const https = require('https');
+var bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const projects = [
+    { name: 'TTS', id: 1 },
+    { name: 'Stress Detection', id: 2 },
+    { name: 'Fall Detection', id: 3 }
+]
+
+app.get("/", (req, res) => {
+    res.send('Team Alpha');
+});
+
+app.get('/api/projects', (req, res) => {
+    res.send(projects);
+});
+
+app.get('/api/projects/:id', (req, res) => {
+    const project = projects.find(c => c.id === parseInt(req.params.id));
+    if (!project) res.status(404).send('Ooops... Cant find what you are looking for!');
+    res.send(project);
+});
+
+app.post('/api/projects', (req, res) => {
+    console.log(req.body);
+    const project = {
+        id: projects.length + 1,
+        name: req.body.name
+    };
+    projects.push(project);
+    res.send(project);
+});
+
+app.put('/api/projects/:id', (req, res) => {
+    const project = projects.find(c => c.id === parseInt(req.params.id));
+
+    if (!project) res.status(404).send('<h2>Not Found!! </h2>');
+
+    project.name = req.body.name;
+    res.send(project);
+});
+
+app.delete('/api/projects/:id', (req, res) => {
+
+    const project = projects.find(c => c.id === parseInt(req.params.id));
+    if (!project) res.status(404).send('<h2> Not Found!! </h2>');
+
+    const index = projects.indexOf(project);
+    projects.splice(index, 1);
+
+    res.send(project);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
