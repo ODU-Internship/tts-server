a9091382ddb3aa3390749654d225952f3d4d9178
Likith <likithsrinath2000@gmail.com>
2021-05-12 14:08:37 +0530

    Fix update Message

--- src/routers/message.js
+++ src/routers/message.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const { getAllMessageController, getMessageController, updateMessageController, insertMessageController } = require('../controllers/MessageController');
+const express = require("express");
+const {
+  getAllMessageController,
+  getMessageController,
+  updateMessageController,
+  insertMessageController,
+} = require("../controllers/MessageController");
 
 const router = express.Router();
 
-router.get('/all', getAllMessageController);
-router.get('/:messageID', getMessageController);
-router.post('/update', updateMessageController);
-router.post('/insert', insertMessageController);
+router.get("/", getAllMessageController);
+router.get("/:messageID", getMessageController);
+router.put("/update/:messageID", updateMessageController);
+router.post("/insert", insertMessageController);
 
 module.exports = router;
