f4f4e265096f02a9cdb00d9889b8cece1d544adb
Likith <likithsrinath2000@gmail.com>
2021-06-04 11:35:02 +0530

    Modify insert train data

--- src/controllers/TrainController.js
+++ src/controllers/TrainController.js
@@ -9,12 +9,12 @@ let TrainData = require("../models/train.model");
 module.exports.insertDataController = [
   async (req, res) => {
     try {
-      const { data } = req.body;
+      // const { data } = req.body;
       const clear_message = await TrainData.clearData();
       if (clear_message == undefined) {
         throw messageNotFoundError("400", "Data could not be deleted");
       }
-      const res_message = await TrainData.insertData(data);
+      const res_message = await TrainData.insertData(req.body);
       if (res_message == undefined) {
         throw messageNotFoundError("400", "Data could not be inserted");
       }
